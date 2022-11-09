4c29e13ec0285f2301289d03886cd3c6eff0c607
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-09 19:15:43 +0530

    Create evenoroddno.cpp

---
+++ codingquestion/evenoroddno.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  for(int i=1;i<=n;i++)
+  {
+    if(i%2==0)
+    {
+      cout<<"even no:";
+    }
+    else
+    {
+      cout<<"odd no:";
+    }
+  }
+  return 0;
+}
+    
