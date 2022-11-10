0c1025abac9aab4b1eb16b78a6df217b86950b5b
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-10 15:40:11 +0530

    Update largestandsmallestelementinarray.cpp

--- codingquestion/largestandsmallestelementinarray.cpp
+++ codingquestion/largestandsmallestelementinarray.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-int n;
+int n,i;
 cin>>n;
-for(int i=1;i<=n;i++)
+for( i=0;i<=n;i++)
 {
   cin>>arr[i];
 }
    int max=arr[0],min=arr[0];
-  for(int i=1;i<=n;i++)
+  for( i=1;i<=n;i++)
   {
     if(arr[i]>max)
     {
