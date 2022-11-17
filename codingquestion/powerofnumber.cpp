c9cff470a35e32cd3bc675f8dc61a5ad6aa4d605
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-17 20:36:46 +0530

    Create powerofnumber.cpp

---
+++ codingquestion/powerofnumber.cpp
@@ -0,0 +1,16 @@
+/* time complexity-
+    space complexity-
+    */
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+  int n,i;
+  cin>>n;
+  cin>>i;
+  int p=pow(n,i);
+  cout<<c;
+  return 0;
+}
+  
