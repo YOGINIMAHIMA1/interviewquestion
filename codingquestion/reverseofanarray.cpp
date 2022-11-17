3dadbb6bd9b78a490e10bc8ba6867aeee3b270c2
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-17 15:01:48 +0530

    Update reverseofanarray.cpp

--- codingquestion/reverseofanarray.cpp
+++ codingquestion/reverseofanarray.cpp
@@ -5,13 +5,17 @@ int main()
 int n;
 cin>>n;
 int arr[n];
-for(int i=1;i<=n;i++)
+for(int i=0;i<n;i++)
 {
   cin>>arr[i];
 }
-for(i=i-1;i>=1;i--)
+for(i=n-1;i>=1;i--)
 {
   cout<<arr[i];
 }
 return 0;
 }
+/*
+time complexity- O(n)
+space complexity-O(1)
+*/
