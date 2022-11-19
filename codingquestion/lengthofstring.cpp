321e57f39a1e3c1fd656ffa1deb0946c136cac4e
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-19 22:32:49 +0530

    Update lengthofstring.cpp

--- codingquestion/lengthofstring.cpp
+++ codingquestion/lengthofstring.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main()
 {
   
-  cout<<sizeof("MahimaBhardwaj")<<endl;
-  cout<<"MahimaBhardwaj"<<endl;
-  cout<<sizeof("M@1#'2\0")<<endl;
-  cout<<"M@1#'2\0"<<endl;
-  cout<<sizeof("He\0llo\0")<<endl;
-  cout<<"He\0llo\0"<<endl;
-  cout<<sizeof("mA1#@%^&*()_+\0")<<endl;
-  cout<<"mA1#@%^&*()_+\0"<<endl;
+  cout<<sizeof("MahimaBhardwaj")<<endl; //15
+  cout<<"MahimaBhardwaj"<<endl; //MahimaBhardwaj
+  cout<<sizeof("M@1#'2\01")<<endl; //9
+  cout<<"M@1#'2\01"<<endl; //M@1#'2
+  cout<<sizeof("He\0llo\0")<<endl; //8
+  cout<<"He\0llo\0"<<endl; //He
+  cout<<sizeof("mA1#@%^&*\0()_+\0")<<endl; //16
+  cout<<"mA1#@%^&*\0()_+\0"<<endl;//mA1#@%^&*
   return 0;
 }
     
