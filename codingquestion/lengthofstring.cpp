4ba4b0ab587818f6368734535779ac11dad5efc3
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-19 22:26:57 +0530

    Update lengthofstring.cpp

--- codingquestion/lengthofstring.cpp
+++ codingquestion/lengthofstring.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 int main()
 {
-  char *str="computer";
-  cout<<sizeof(str);
+  
+  cout<<sizeof("MahimaBhardwaj")<<endl;
+  cout<<"MahimaBhardwaj"<<endl;
+  cout<<sizeof("M@1#'2\0")<<endl;
+  cout<<"M@1#'2\0"<<endl;
+  cout<<sizeof("He\0llo\0")<<endl;
+  cout<<"He\0llo\0"<<endl;
+  cout<<sizeof("mA1#@%^&*()_+\0")<<endl;
+  cout<<"mA1#@%^&*()_+\0"<<endl;
   return 0;
 }
     
